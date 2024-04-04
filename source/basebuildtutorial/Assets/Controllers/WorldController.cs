8dab674bb7d5f803ad344c9e9b72e1d2155cd6cb
bluekigh <bluekigh@gmail.com>
2024-04-04 16:57:37 +0900

    ep3

--- source/basebuildtutorial/Assets/Controllers/WorldController.cs
+++ source/basebuildtutorial/Assets/Controllers/WorldController.cs
@@ -1,19 +1,69 @@
-﻿using UnityEngine;
+﻿//=======================================================================
+// Copyright Martin "quill18" Glaude 2015.
+//		http://quill18.com
+//=======================================================================
+
+
+using UnityEngine;
 using System.Collections;
 
 public class WorldController : MonoBehaviour {
 
+	// The only tile sprite we have right now, so this
+	// it a pretty simple way to handle it.
+	public Sprite floorSprite;
+
+	// The world and tile data
 	World world;
 
 	// Use this for initialization
 	void Start () {
+		// Create a world with Empty tiles
 		world = new World();
 
-	}
+		// Create a GameObject for each of our tiles, so they show visually. (and redunt reduntantly)
+		for (int x = 0; x < world.Width; x++) {
+			for (int y = 0; y < world.Height; y++) {
+				// Get the tile data
+				Tile tile_data = world.GetTileAt(x, y);
 
+				// This creates a new GameObject and adds it to our scene.
+				GameObject tile_go = new GameObject();
+				tile_go.name = "Tile_" + x + "_" + y;
+				tile_go.transform.position = new Vector3( tile_data.X, tile_data.Y, 0);
+
+				// Add a sprite renderer, but don't bother setting a sprite
+				// because all the tiles are empty right now.
+				tile_go.AddComponent<SpriteRenderer>();
+
+				// Use a lambda to create an anonymous function to "wrap" our callback function
+				tile_data.RegisterTileTypeChangedCallback( (tile) => { OnTileTypeChanged(tile, tile_go); } );
+			}
+		}
+
+		// Shake things up, for testing.
+		world.RandomizeTiles();
+	}
 
 	// Update is called once per frame
 	void Update () {
+
+	}
+
+	// This function should be called automatically whenever a tile's type gets changed.
+	void OnTileTypeChanged(Tile tile_data, GameObject tile_go) {
+
+		if(tile_data.Type == Tile.TileType.Floor) {
+			tile_go.GetComponent<SpriteRenderer>().sprite = floorSprite;
+		}
+		else if( tile_data.Type == Tile.TileType.Empty ) {
+			tile_go.GetComponent<SpriteRenderer>().sprite = null;
+		}
+		else {
+			Debug.LogError("OnTileTypeChanged - Unrecognized tile type.");
+		}
+
+
 	}
 
 }
--- source/basebuildtutorial/Assets/Models/LooseObject.cs
+++ source/basebuildtutorial/Assets/Models/LooseObject.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿//=======================================================================
+// Copyright Martin "quill18" Glaude 2015.
+//		http://quill18.com
+//=======================================================================
+
+using UnityEngine;
 using System.Collections;
 
 public class LooseObject {
